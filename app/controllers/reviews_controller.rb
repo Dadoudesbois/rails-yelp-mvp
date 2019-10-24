c304f4fba4e1e29f0f7759d99dc19972b7902a64
Daphne Laboul <daphne.laboul@gmail.com>
2019-10-24 12:27:49 +0200

    better resto app

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,13 +9,13 @@ class ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
     @review.save
-    redirect_to show_path(@restaurant)
+    redirect_to restaurant_path(@restaurant)
   end
 
   private
 
   def review_params
-    params.require(:review).permit(:content, :rating)
+    params.require(:review).permit(:content, :rating, :restaurant)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,6 @@
 Rails.application.routes.draw do
-  get 'restaurants', to: 'restaurants#index', as: :index
-
-  get 'restaurants/new', to: 'restaurants#new'
-  post 'restaurants', to: 'restaurants#create'
-
-  get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
-
-  get 'restaurants/:restaurant_id/reviews/new', to: 'reviews#new', as: :new_review
-  post 'restaurants/:restaurant_id/reviews', to: 'reviews#create', as: :restaurant_reviews
-  # get 'restaurants/:id/reviews', to: 'reviews#show', as: :show_reviews
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'restaurants#index'
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:create]
+  end
 end
